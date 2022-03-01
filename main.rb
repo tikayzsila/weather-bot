dfa0a3119a002ed1f33c92ba2c7496c11b980b66
tikayzsila <kwetrus@yandex.ru>
2022-03-01 15:21:19 +0300

    working bot withot json file

--- main.rb
+++ main.rb
@@ -8,9 +8,27 @@ if Gem.win_platform?
 end
 
 require 'telegram/bot'
+require 'uri-handler'
+require 'rexml/document'
+require 'uri'
+require 'net/http'
+require 'json'
+require 'debug'
+require_relative 'Forecast'
 #require_relative 'weather'
 
 
+uri = URI.parse("https://xml.meteoservice.ru/export/gismeteo/point/135.xml")
+response = Net::HTTP.get_response(uri)
+doc = REXML::Document.new(response.body)
+
+city_name = doc.root.elements['REPORT/TOWN'].attributes['sname']
+
+forecast_nodes = doc.root.elements['REPORT/TOWN'].elements.to_a
+
+#forecast_nodes.each do |node|
+ #Forecast.from_xml(node)
+#end
 
 TOKEN = "5275892175:AAHR22gLPKIDKne8Lm6ML_kuE_k7FB8R1cI"
 
@@ -21,20 +39,32 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
     case message
     when Telegram::Bot::Types::CallbackQuery
       # Here you can handle your callbacks from inline buttons
+      
       if message.data == 'Moscow'
-        bot.api.send_message(chat_id: message.from.id, text: "HUETA")
-      elsif
-        message.data == 'Rostov'
-        bot.api.send_message(chat_id: message.from.id, text: "HUETA")
-      elsif
-        message.data == 'Samara'
-        bot.api.send_message(chat_id: message.from.id, text: "HUETA")
-      elsif
-        message.data == 'Peterburg'
-        bot.api.send_message(chat_id: message.from.id, text: "HUETA")
-      elsif
-        message.data == 'Steel'
-        bot.api.send_message(chat_id: message.from.id, text: "HUETA")
+      bot.api.send_message(chat_id: message.from.id, text: "#{city_name.uri_decode}")
+        forecast_nodes.each do |node|
+        bot.api.send_message(chat_id: message.from.id, text: "#{Forecast.from_xml(node)}")
+        end
+      elsif message.data == 'Rostov'
+        bot.api.send_message(chat_id: message.from.id, text: "#{city_name.uri_decode}")
+        forecast_nodes.each do |node|
+        bot.api.send_message(chat_id: message.from.id, text: "#{Forecast.from_xml(node)}")
+        end
+      elsif message.data == 'Samara'
+        bot.api.send_message(chat_id: message.from.id, text: "#{city_name.uri_decode}")
+        forecast_nodes.each do |node|
+        bot.api.send_message(chat_id: message.from.id, text: "#{Forecast.from_xml(node)}")
+        end
+      elsif message.data == 'Peterburg'
+        bot.api.send_message(chat_id: message.from.id, text: "#{city_name.uri_decode}")
+        forecast_nodes.each do |node|
+        bot.api.send_message(chat_id: message.from.id, text: "#{Forecast.from_xml(node)}")
+        end
+      elsif message.data == 'Steel'
+        bot.api.send_message(chat_id: message.from.id, text: "#{city_name.uri_decode}")
+        forecast_nodes.each do |node|
+        bot.api.send_message(chat_id: message.from.id, text: "#{Forecast.from_xml(node)}")
+        end
       end
     when Telegram::Bot::Types::Message
       kb = [
--- weather.rb
+++ weather.rb
@@ -1,11 +1,4 @@
-if Gem.win_platform?
-  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
-  Encoding.default_internal = __ENCODING__
 
-  [STDIN, STDOUT].each do |io|
-    io.set_encoding(Encoding.default_external, Encoding.default_internal)
-  end
-end
 
 require 'rexml/document'
 require 'uri'
@@ -15,23 +8,23 @@ require 'json'
 require 'debug'
 require_relative 'Forecast'
 
-file = File.read('cities.json', encoding: "UTF-8")
-city_names = JSON.parse(file)
+#file = File.read('cities.json', encoding: "UTF-8")
+#city_names = JSON.parse(file)
 
-puts "У нас в базе есть #{city_names.keys.size} городов:"
-puts city_names.keys
+#puts "У нас в базе есть #{city_names.keys.size} городов:"
+#puts city_names.keys
 
-puts 'Погоду для какого города Вы хотите узнать?'
-city_index = STDIN.gets.chomp
+#puts 'Погоду для какого города Вы хотите узнать?'
+#city_index = STDIN.gets.chomp
 #binding.break
 
-if city_names.has_key?(city_index)
-city = city_names[city_index]
+#if city_names.has_key?(city_index)
+#city = city_names[city_index]
 #binding.break
-uri = URI.parse("https://xml.meteoservice.ru/export/gismeteo/point/#{city["id"]}.xml")
-else 
-abort 'Извините, этот город ещё не добавлен в нашу базу данных' 
-end
+uri = URI.parse("https://xml.meteoservice.ru/export/gismeteo/point/135.xml")
+#else 
+#abort 'Извините, этот город ещё не добавлен в нашу базу данных' 
+#end
 
 response = Net::HTTP.get_response(uri)
 doc = REXML::Document.new(response.body)
@@ -40,8 +33,12 @@ city_name = doc.root.elements['REPORT/TOWN'].attributes['sname']
 
 forecast_nodes = doc.root.elements['REPORT/TOWN'].elements.to_a
 
-
 forecast_nodes.each do |node|
 puts city_name.uri_decode
-puts Forecast.from_xml(node)
+a = Forecast.from_xml(node)
+binding.break
+return a
 end
+end
+binding.break
+puts a
